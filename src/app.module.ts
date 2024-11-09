c1a9f273d6cf3108012e13f441b81fd0049fa2ca
Sombo20 <vicentevictorsombo643@gmail.com>
2024-11-09 17:12:31 +0100

    chore:add rate-limit to all api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,32 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { ServicesModule } from './services/services.module';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import * as dotenv from 'dotenv';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 dotenv.config();
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 50,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 100,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 1000,
+      },
+    ]),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
@@ -26,6 +43,12 @@ dotenv.config();
     UsersModule,
     ReservationsModule,
     ServicesModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
