0aac1ca02357dc60fa7277b9c8e5e60b3c97f7b0
Sombo20 <vicentevictorsombo643@gmail.com>
2024-11-10 21:48:13 +0100

    chore:add new mysql port

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,7 +32,7 @@ dotenv.config();
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: process.env.MYSQL_HOST,
-      port: 3306,
+      port: 17549,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
