a2fd756c0d75cd17bb5282748b0f61077f04335b
Sombo20 <vicentevictorsombo643@gmail.com>
2024-11-11 04:44:16 +0100

    chore:change jwt return on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,8 +24,6 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id, role: user.role };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+    return this.jwtService.sign(payload);
   }
 }
